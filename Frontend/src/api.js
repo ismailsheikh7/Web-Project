9a1ca6f3a09567ceef99642cb21137c864340dfb
Sheikh I <sheikh.ismail80@yahoo.com>
2022-06-15 12:12:28 +0500

    Done

---
+++ Frontend/src/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const url= "http://localhost:9002/ecommerce";
+
+
+export const AddItem = async (ItemData) => {
+    return await axios.post (`${url}/AddItem`, ItemData);
+    
+};
+
+export const UpdateItem= async (ItemData) => {
+    return await axios.put (`${url}/UpdateItem`, ItemData);
+};
+
+export const DeleteItem= async (ItemData) => {
+    return await axios.delete (`${url}/DeleteItem/${ItemData.ItemId}`)
+};
+
+export const GetItem = async () => {
+    return await axios.get(`${url}/ViewItem`);
+};
+
